cb2e8bc20f4a8d9ddcf69cd8ba4abfe413971ec0
surya <surybhan2002@gmail.com>
2025-03-24 15:40:28 -0700

    abcde

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import contact_view, success_view
+from .views import contact_view, success_view, home
 
 urlpatterns = [
+    path("",home, name="home"),
     path('contact/', contact_view, name='contact'),
     path('success/', success_view, name='success'),
 ]
